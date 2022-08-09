68822c61c147ef34ff859919cd9cb18ca93382b5
Auden Vitasa <87536767+abvitasa@users.noreply.github.com>
2022-08-09 18:39:35 +0800

    Update skinstaller.ps1

--- skinstaller.ps1
+++ skinstaller.ps1
@@ -1,8 +1,8 @@
-# # Install msys2
-# & "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install msys2
+# Install msys2
+& "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install msys2
 
-# # Install VS Code
-# & "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install Microsoft.VisualStudioCode
+# Install VS Code
+& "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install Microsoft.VisualStudioCode
 
 # Installing the SplashKit SDK
 function InstallSplashKit {
@@ -10,16 +10,16 @@ function InstallSplashKit {
     # Install git
     pacman -S git --noconfirm --disable-download-timeout
 
-    # Install SplashKit (-O,  --output-document=FILE      write documents to FILE)
-#     wget https://raw.githubusercontent.com/splashkit/skm/master/install-scripts/skm-install.sh -O skm-install.sh
-#     bash skm-install.sh
-#     rm skm-install.sh
+    # Install SplashKit
+    wget https://raw.githubusercontent.com/splashkit/skm/master/install-scripts/skm-install.sh -O skm-install.sh
+    bash skm-install.sh
+    rm skm-install.sh
 
-#     # Installing GCC on Windows for C++
-#     bash -l -c "yes | pacman --disable-download-timeout -S mingw-w64-{x86_64,i686}-gcc mingw-w64-{i686,x86_64}-gdb"
+    # Installing GCC on Windows for C++
+    bash -l -c "yes | pacman --disable-download-timeout -S mingw-w64-{x86_64,i686}-gcc mingw-w64-{i686,x86_64}-gdb"
 }
 
 InstallSplashKit
 
-# # Install the .NET 6 LTS Core SDK on Windows for C#
-# & "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install Microsoft.DotNet.SDK.6 
+# Install the .NET 6 LTS Core SDK on Windows for C#
+& "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget" install Microsoft.DotNet.SDK.6 
